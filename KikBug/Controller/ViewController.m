a1ab50ba04d0bcec16a2bc92540c77fdc108a866
DamonLiuNJU <liu394000785@sina.com>
2015-11-10 13:57:21 +0800

    Login page development

--- KikBug/Controller/ViewController.m
+++ KikBug/Controller/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userId;
+@property (weak, nonatomic) IBOutlet UITextField *password;
 
 @end
 
@@ -17,11 +19,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UITapGestureRecognizer* rec = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(closeKeyboard)];
+    [self.view addGestureRecognizer:rec];
+}
+
+
+-(void)closeKeyboard
+{
+    [self.userId resignFirstResponder];
+    [self.password resignFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
