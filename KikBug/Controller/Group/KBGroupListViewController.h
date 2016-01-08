858bc5f9122294c066b5bb791977a3ff63162257
DamonLiuNJU <liu394000785@sina.com>
2016-01-08 15:35:22 +0800

    new group files

---
+++ KikBug/Controller/Group/KBGroupListViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBGroupListViewController.h
+//  KikBug
+//
+//  Created by DamonLiu on 16/1/8.
+//  Copyright © 2016年 DamonLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBGroupListViewController : UIViewController
+
+@end
---
+++ KikBug/Controller/Group/KBGroupListViewController.m
@@ -0,0 +1,13 @@
+//
+//  KBGroupListViewController.m
+//  KikBug
+//
+//  Created by DamonLiu on 16/1/8.
+//  Copyright © 2016年 DamonLiu. All rights reserved.
+//
+
+#import "KBGroupListViewController.h"
+
+@implementation KBGroupListViewController
+
+@end
