cffd89965e19d251670fcefbcd89da9161a13470
DamonLiuNJU <liu394000785@sina.com>
2016-03-23 16:10:34 +0800

    注释信息

--- KikBug/Model/ReportManager/KBReportData.h
+++ KikBug/Model/ReportManager/KBReportData.h
@@ -11,15 +11,15 @@
 @interface KBTaskReport : KBBaseModel
 
 JSONINT taskId;
-JSONSTIRNG name;
-JSONSTIRNG logLocation;
-JSONSTIRNG reportLocation;
-JSONSTIRNG mobileBrand;
-JSONSTIRNG mobileModel;
-JSONSTIRNG mobileOs;
-@property (assign,nonatomic) BOOL bugFound;
-JSONINT timeUsed;
-JSONSTIRNG stepDescription;
+JSONSTIRNG name;           //报告名称
+JSONSTIRNG logLocation;    //系统生成的
+JSONSTIRNG reportLocation; //
+JSONSTIRNG mobileBrand;//苹果
+JSONSTIRNG mobileModel;//iPhone 6s
+JSONSTIRNG mobileOs;   //
+@property (assign,nonatomic) NSInteger bugFound;// 发现的bug数量 = 0
+JSONINT timeUsed; //以秒的形式
+JSONSTIRNG stepDescription; //整个报告的描述 @『等待补充』
 
 + (instancetype)fakeReport;
 
