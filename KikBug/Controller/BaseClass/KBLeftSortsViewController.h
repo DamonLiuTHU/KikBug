914320a476360b71063bb468f1ef9a89635d8d93
DamonLiu <liu394000785@sina.com>
2016-05-27 12:49:19 +0800

    move files

---
+++ KikBug/Controller/BaseClass/KBLeftSortsViewController.h
@@ -0,0 +1,16 @@
+//
+//  KBLeftSortsViewController.h
+//  KBLGDeckViewController
+//
+//  Created by jamie on 15/3/31.
+//  Copyright (c) 2015年 Jamie-Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBLeftSortsViewController : UIViewController
+
+@property (nonatomic,strong) UITableView *tableview;
+@property (strong,nonatomic) NSArray<UIViewController *> *viewControllers;
+
+@end
---
+++ KikBug/Controller/BaseClass/KBLeftSortsViewController.m
@@ -0,0 +1,81 @@
+//
+//  KBLeftSortsViewController.m
+//  KBLGDeckViewController
+//
+//  Created by jamie on 15/3/31.
+//  Copyright (c) 2015年 Jamie-Ling. All rights reserved.
+//
+
+#import "KBLeftSortsViewController.h"
+#import "AppDelegate.h"
+
+@interface KBLeftSortsViewController () <UITableViewDelegate,UITableViewDataSource>
+
+@end
+
+@implementation KBLeftSortsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UIImageView *imageview = [[UIImageView alloc] initWithFrame:self.view.bounds];
+    imageview.image = [UIImage imageNamed:@"leftbackiamge"];
+    [self.view addSubview:imageview];
+
+    UITableView *tableview = [[UITableView alloc] init];
+    self.tableview = tableview;
+    tableview.frame = self.view.bounds;
+    tableview.dataSource = self;
+    tableview.delegate  = self;
+    tableview.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self.view addSubview:tableview];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.viewControllers.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *Identifier = @"Identifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:Identifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:Identifier];
+    }
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.textLabel.font = [UIFont systemFontOfSize:20.0f];
+    cell.backgroundColor = [UIColor clearColor];
+    cell.textLabel.textColor = [UIColor whiteColor];
+    
+    cell.textLabel.text = self.viewControllers[indexPath.row].title;
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+//    AppDelegate *tempAppDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    UIViewController *vc = self.viewControllers[indexPath.row];
+    [UIManager.LeftSlideVC closeLeftView];//关闭左侧抽屉
+//    [tempAppDelegate.mainNavigationController pushViewController:vc animated:NO];
+    [UIManager showViewController:vc];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 180;
+}
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.tableview.bounds.size.width, 180)];
+    view.backgroundColor = [UIColor clearColor];
+    return view;
+}
+@end
